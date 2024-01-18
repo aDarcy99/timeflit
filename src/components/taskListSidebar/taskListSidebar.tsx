ddb94c107b47f259afdb7325bebb728f06f0a349
lachlan darcy <darcylachlan@hotmail.com.au>
2024-01-18 20:35:26 +1100

    feat(*) replaced delete button with kebab menu that opens up options for task list feat(*) added duplicate option for task list

--- src/components/taskListSidebar/taskListSidebar.tsx
+++ src/components/taskListSidebar/taskListSidebar.tsx
@@ -1,22 +1,31 @@
-import React, { MouseEvent, useContext, useEffect, useState } from 'react';
+import React, { MouseEvent, useContext, useEffect, useState } from "react";
 // Functions
-import clsx from 'clsx';
-import { TaskContext } from '../../contexts/taskContext';
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
-import { TaskListBrowserContext } from '../../contexts/taskListBrowserContext';
+import clsx from "clsx";
+import { TaskContext } from "../../contexts/taskContext";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { TaskListBrowserContext } from "../../contexts/taskListBrowserContext";
 // Components
-import Button from '../reusable/button/button';
-import DeleteTaskListConfirmationModal from '../modals/deleteTaskListConfirmationModal/deleteTaskListConfirmationModal';
-import OpenFileConfirmationModal from '../modals/openFileConfirmationModal/openFileConfirmationModal';
+import Button from "../reusable/button/button";
+import DeleteTaskListConfirmationModal from "../modals/deleteTaskListConfirmationModal/deleteTaskListConfirmationModal";
+import OpenFileConfirmationModal from "../modals/openFileConfirmationModal/openFileConfirmationModal";
 // Assets
-import DeleteIcon from '../../assets/svgs/deleteIcon';
-import NoteIcon from '../../assets/svgs/noteIcon';
-import PlusIcon from '../../assets/svgs/plusIcon';
-import ArrowIcon from '../../assets/svgs/arrowIcon';
-import DownloadIcon from '../../assets/svgs/downloadIcon';
-import FolderIcon from '../../assets/svgs/folderIcon';
+import DeleteIcon from "../../assets/svgs/deleteIcon";
+import NoteIcon from "../../assets/svgs/noteIcon";
+import PlusIcon from "../../assets/svgs/plusIcon";
+import ArrowIcon from "../../assets/svgs/arrowIcon";
+import DownloadIcon from "../../assets/svgs/downloadIcon";
+import FolderIcon from "../../assets/svgs/folderIcon";
 // Styles
-import classes from './taskListSidebar.module.scss';
+import classes from "./taskListSidebar.module.scss";
+import Menu from "../reusable/menu/menu";
+import MenuItem from "../reusable/menu/menuItem/menuItem";
+import CopyIcon from "../../assets/svgs/copyIcon";
+import KebabIcon from "../../assets/svgs/kebabIcon";
 
 type TTaskListSidebarProps = {
   isOpen: boolean;
@@ -24,13 +33,25 @@ type TTaskListSidebarProps = {
 };
 
 const TaskListSidebar = ({ onClose, isOpen }: TTaskListSidebarProps) => {
-  const { taskLists, createSingleTaskList, updateTaskListItemArrayIndex, saveTaskListsToDisk } = useContext(TaskContext);
+  const {
+    taskLists,
+    createSingleTaskList,
+    updateTaskListItemArrayIndex,
+    duplicateSingleTaskListById,
+    saveTaskListsToDisk,
+  } = useContext(TaskContext);
   const { addNewOpenTaskListId } = useContext(TaskListBrowserContext);
 
   const [isSidebarMinimized, setIsSideBarMinimized] = useState(false);
-  const [isDeleteTaskListModalOpen, setIsDeleteTaskListModalOpen] = useState(false);
+  const [isDeleteTaskListModalOpen, setIsDeleteTaskListModalOpen] =
+    useState(false);
   const [isOpenFileModalOpen, setIsOpenFileModalOpen] = useState(false);
-  const [deleteTaskId, setDeleteTaskId] = useState<string | undefined>(undefined);
+  const [deleteTaskId, setDeleteTaskId] = useState<string | undefined>(
+    undefined
+  );
+  const [currentSettingsMenuOpen, setCurrentSettingsMenuOpened] = useState<
+    string | undefined
+  >(undefined);
 
   const onToggleSidebarButtonClick = () => {
     setIsSideBarMinimized(!isSidebarMinimized);
@@ -49,11 +70,29 @@ const TaskListSidebar = ({ onClose, isOpen }: TTaskListSidebarProps) => {
     };
   };
 
-  const onDeleteTaskListButtonClick = (taskListId: string) => {
+  const onSettingsTaskListButtonClick = (taskListId: string) => {
     return (e: MouseEvent<HTMLButtonElement>) => {
       e.stopPropagation();
+      setCurrentSettingsMenuOpened((currentSettingsMenuOpen) =>
+        currentSettingsMenuOpen === undefined ? taskListId : undefined
+      );
+    };
+  };
+
+  const onDeleteTaskListButtonClick = (taskListId: string) => {
+    return (e: MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
       setIsDeleteTaskListModalOpen(true);
+      setCurrentSettingsMenuOpened(undefined);
       setDeleteTaskId(taskListId);
+    };
+  };
+
+  const onDuplicateTaskListButtonClick = (taskListId: string) => {
+    return (e: MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+      duplicateSingleTaskListById(taskListId);
+      setCurrentSettingsMenuOpened(undefined);
     };
   };
 
@@ -86,71 +125,119 @@ const TaskListSidebar = ({ onClose, isOpen }: TTaskListSidebarProps) => {
 
   return (
     <>
-      <nav className={clsx(classes['root'], isSidebarMinimized && classes['is-minimized'])}>
-        <div className={classes['sidebar-content']}>
-          <h2 className={classes['heading']}>Timeflit</h2>
-          <div className={classes['controls']}>
+      <nav
+        className={clsx(
+          classes["root"],
+          isSidebarMinimized && classes["is-minimized"]
+        )}
+      >
+        <div className={classes["sidebar-content"]}>
+          <h2 className={classes["heading"]}>Timeflit</h2>
+          <div className={classes["controls"]}>
             <Button
-              className={classes['open-file-button']}
-              width='fit-container'
-              textAlignment='start'
-              variant='transparent'
-              color='grey'
+              className={classes["open-file-button"]}
+              width="fit-container"
+              textAlignment="start"
+              variant="transparent"
+              color="grey"
               onClick={onOpenFileButtonClick}
             >
               <FolderIcon />
               Open from file
             </Button>
             <Button
-              className={classes['save-file-button']}
-              width='fit-container'
-              textAlignment='start'
-              variant='transparent'
-              color='grey'
+              className={classes["save-file-button"]}
+              width="fit-container"
+              textAlignment="start"
+              variant="transparent"
+              color="grey"
               onClick={onSaveFileButtonClick}
             >
               <DownloadIcon />
               Save to file
             </Button>
             <Button
-              className={classes['new-task-list-button']}
-              width='fit-container'
-              textAlignment='start'
-              variant='transparent'
-              color='grey'
+              className={classes["new-task-list-button"]}
+              width="fit-container"
+              textAlignment="start"
+              variant="transparent"
+              color="grey"
               onClick={onNewTaskListButtonClick}
             >
               <PlusIcon />
               New task list
             </Button>
           </div>
-          <h2 className={classes['heading']}>Task Lists</h2>
-          <div className={classes['task-list-container']}>
+          <h2 className={classes["heading"]}>Task Lists</h2>
+          <div className={classes["task-list-container"]}>
             <DragDropContext onDragEnd={onDragEnd}>
-              <Droppable droppableId='task-droppable'>
+              <Droppable droppableId="task-droppable">
                 {(provided) => (
-                  <div {...provided.droppableProps} ref={provided.innerRef} className={classes['task-list']}>
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className={classes["task-list"]}
+                  >
                     {taskLists.map((taskList, idx) => (
-                      <Draggable key={taskList.id} draggableId={taskList.id} index={idx}>
+                      <Draggable
+                        key={taskList.id}
+                        draggableId={taskList.id}
+                        index={idx}
+                      >
                         {(provided) => (
                           <div
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                             ref={provided.innerRef}
-                            className={classes['task-list-item']}
+                            className={clsx(
+                              classes["task-list-item"],
+                              currentSettingsMenuOpen === taskList.id &&
+                                classes["is-focused"]
+                            )}
                             tabIndex={0}
                             onClick={onOpenTaskListButtonClick(taskList.id)}
                           >
                             {taskList.name}
-                            <Button
-                              className={classes['delete-button']}
-                              iconOnly
-                              variant='transparent'
-                              color='grey'
-                              onClick={onDeleteTaskListButtonClick(taskList.id)}
+                            <Menu
+                              trigger="custom"
+                              position="bottom"
+                              targetElement={
+                                <Button
+                                  className={classes["settings-button"]}
+                                  iconOnly
+                                  variant="transparent"
+                                  color="grey"
+                                  onClick={onSettingsTaskListButtonClick(
+                                    taskList.id
+                                  )}
+                                >
+                                  <KebabIcon />
+                                </Button>
+                              }
+                              isOpen={currentSettingsMenuOpen === taskList.id}
+                              onClose={onSettingsTaskListButtonClick(
+                                taskList.id
+                              )}
                             >
-                              <DeleteIcon />
-                            </Button>
+                              <MenuItem
+                                className={classes["duplicate-button"]}
+                                onClick={onDuplicateTaskListButtonClick(
+                                  taskList.id
+                                )}
+                              >
+                                <CopyIcon />
+                                Duplicate
+                              </MenuItem>
+                              <MenuItem
+                                className={classes["delete-button"]}
+                                onClick={onDeleteTaskListButtonClick(
+                                  taskList.id
+                                )}
+                              >
+                                <DeleteIcon />
+                                Delete
+                              </MenuItem>
+                            </Menu>
                           </div>
                         )}
                       </Draggable>
@@ -163,12 +250,28 @@ const TaskListSidebar = ({ onClose, isOpen }: TTaskListSidebarProps) => {
           </div>
         </div>
 
-        <Button className={classes['minimize-button']} iconOnly color='grey' variant='transparent' onClick={onToggleSidebarButtonClick}>
-          <ArrowIcon size='lg' direction={isSidebarMinimized ? 'right' : 'left'} />
+        <Button
+          className={classes["minimize-button"]}
+          iconOnly
+          color="grey"
+          variant="transparent"
+          onClick={onToggleSidebarButtonClick}
+        >
+          <ArrowIcon
+            size="lg"
+            direction={isSidebarMinimized ? "right" : "left"}
+          />
         </Button>
       </nav>
-      <DeleteTaskListConfirmationModal isOpen={isDeleteTaskListModalOpen} taskListId={deleteTaskId} onClose={onDeleteTaskModalClose} />
-      <OpenFileConfirmationModal isOpen={isOpenFileModalOpen} onClose={onOpenFileModalClose} />
+      <DeleteTaskListConfirmationModal
+        isOpen={isDeleteTaskListModalOpen}
+        taskListId={deleteTaskId}
+        onClose={onDeleteTaskModalClose}
+      />
+      <OpenFileConfirmationModal
+        isOpen={isOpenFileModalOpen}
+        onClose={onOpenFileModalClose}
+      />
     </>
   );
 };
